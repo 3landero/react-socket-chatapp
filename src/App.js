0736d3a792b02b57c34ff7dc0b099bb3e9b3b3da
Ramses Landero <27landero@gmail.com>
2022-08-29 01:51:57 -0500

    ok to deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import io from 'socket.io-client'
 import Chat from './Chat';
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://enigmatic-brook-31531.herokuapp.com/")
 
 const App=()=>{
   const [username, setUsername] = useState("")
